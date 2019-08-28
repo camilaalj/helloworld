d0df80c84c978f156afdca068fe30a04ecc35104
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-08-28 09:59:14 -0500

    resuelve la iteración

---
+++ Mayordetresnumeros/Iterador/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /** 1°
+    float a;
+    cout<< "Ingrese un numero:" << endl;
+    cin>> a;
+    while (a > 1){
+        a = a/2;
+    }
+    if (a == 1.0)
+        cout << " Es un potencia de 2"<< endl;
+    else
+        cout << " No es potencia de 2"<< endl;*/
+
+    /** 2°
+    int a; int b= 1;
+    cout<< "Ingrese un numero:" << endl;
+    cin>> a;
+    while (b <= a){
+        a = a/b;
+        b++;
+    }
+    if (a == 0.0)
+        cout<< "No un numero primo"<<endl;
+    else
+        cout<<"Es un numero primo"<<endl;*/
+
+    /** 4°*/
+
+
+
+    return 0;
+}
