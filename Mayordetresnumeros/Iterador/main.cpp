13c48269add4e4123b8658ed1ab2f0ff3312b0fb
camilaalj <camilaalj6@gmail.com>
2019-09-03 17:52:09 -0500

    Resuelve la iteracion

--- Mayordetresnumeros/Iterador/main.cpp
+++ Mayordetresnumeros/Iterador/main.cpp
@@ -16,22 +16,75 @@ int main()
     else
         cout << " No es potencia de 2"<< endl;*/
 
-    /** 2°
-    int a; int b= 1;
+    /**2°
+    int a; int b= 2; int c=0;
     cout<< "Ingrese un numero:" << endl;
     cin>> a;
-    while (b <= a){
-        a = a/b;
+    while (a>b){
+        if (a%b == 0)
+            c++;
         b++;
     }
-    if (a == 0.0)
-        cout<< "No un numero primo"<<endl;
+    if (c==0){
+        cout<<"Es un numero primo"<<endl;
+    }
     else
-        cout<<"Es un numero primo"<<endl;*/
+        cout<< "No es un numero primo"<<endl;
+    return 0;
 
-    /** 4°*/
+    int a; int b= 2; int c=0;
+    cout<< "Ingrese un numero:" << endl;
+    cin>> a;
+    for (int b=2; a > b; b++){
+        if (a%b == 0)
+            c++;
+    }
+    if (c==0)
+        cout<< "Es un numero primo"<<endl;
+    else
+        cout<< "No es un numero primo"<<endl;
+    return 0;*/
+    /**3°*/
+    int a=2;int b =2;int c=0;
+    while (a<100){
+        while (a>b){
+            if (a%b == 0)
+                c++;
+            b++;
+        if (c==0)
+            cout<<a<<endl;
+        }
+        a++;
+        c=0;
+    }
 
+    /** 4°
 
+    int a; int b = 1; int c = 0;
+    cout<< "Ingrese un numero:" << endl;
+    cin>> a;
+    while (a>b){
+        if (a%b==0)
+            c=c+b;
+        b++;
+    }
+    if (c==a)
+        cout<<a<< " Es un numero perfecto"<< endl;
+    else
+        cout<<a<<" No es un numero perfecto"<< endl;
+
+    return 0;
+    int a; int b = 1; int c = 0;
+    cout<< "Ingrese un numero:" << endl;
+    cin>> a;
+    for (int b=1; a>b; b++){
+        if (a%b==0)
+            c=c+b;
+    }
+    if (c==a)
+        cout<<a<< " Es un numero perfecto"<< endl;
+    else
+        cout<<a<<" No es un numero perfecto"<< endl;*/
 
     return 0;
 }
