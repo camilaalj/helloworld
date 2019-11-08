935618170f208d4887d3724efa6bbda11c00780a
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-11-08 13:50:27 -0500

    Herencia

--- Herencia/main.cpp
+++ Herencia/main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 class Trabajo{
+
+    protected:
+        double sueldo;
+        int horastrabajo;
+
     public:
         Trabajo();
         Trabajo(double sueldo, int horastrabajo){
@@ -13,31 +18,33 @@ class Trabajo{
             cout<<"Su suedo es: "<<sueldo<<endl;
             cout<<"Trabaja: "<<horastrabajo<<" horas"<<endl;
         }
-    protected:
-        double sueldo;
-        int horastrabajo;
 };
 
 class Doctor :public Trabajo{
+
+    private:
+        int pacientesdiarios;
+
     public:
         Doctor(double _sueldo,int _horastrabajo, int _pacientesdiarios):
             Trabajo(_sueldo, _horastrabajo),pacientesdiarios(_pacientesdiarios){}
         void Anadir(){
             cout<<"Atiende a: "<<pacientesdiarios<<" pacientes"<<endl;
         }
-    private:
-        int pacientesdiarios;
+
 };
 
 class Gasfitero :public Trabajo{
+
+    private:
+        string problemamascomun;
+
     public:
         Gasfitero(double _sueldo,int _horastrabajo, string _problemamascomun):
             Trabajo(_sueldo, _horastrabajo),problemamascomun(_problemamascomun){}
         void Anadir(){
             cout<<"Su problema mas comun es: "<<problemamascomun<<endl;
         }
-    private:
-        string problemamascomun;;
 };
 
 int main()
@@ -45,10 +52,17 @@ int main()
     Doctor a(3200,12,25);
     a.mostrarDatos();
     a.Anadir();
+    Doctor *ptr=&a;
+    ptr->mostrarDatos();
+    ptr->Anadir();
     cout<<endl;
+
     Gasfitero b(3000,10,"canerias");
     b.mostrarDatos();
     b.Anadir();
+    Gasfitero *pt=&b;
+    pt->mostrarDatos();
+    pt->Anadir();
     return 0;
 }
 
